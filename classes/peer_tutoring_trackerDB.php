0bde45806259c42626a8d68c8e4d3eaad019c67a
johnysaavedra <johnyjanesamsonsaavedra@gmail.com>
2026-05-11 00:55:39 +0800

    Add files via upload

---
+++ classes/peer_tutoring_trackerDB.php
@@ -0,0 +1,13 @@
+<?php
+$host   = "localhost";
+$dbname = "peer_tutoring_tracker";
+$user   = "root";
+$pass   = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
